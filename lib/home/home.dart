bba31b62899c2c1412f739b4c6af4e89b6c5d958
Ben Martin <the.one.martin@gmail.com>
2021-11-19 21:44:10 -0500

    Add stream for auth and some shared widgets

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/shared/shared.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/topics/topics.dart';
+import 'package:quizapp/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text(
-            'click', 
-            style: Theme.of(context).textTheme.button,
-          ),
-          onPressed: () => Navigator.pushNamed((context), '/topics'),
-        ),
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text(
+              'loading',
+              textDirection: TextDirection.ltr,
+            );
+          }
+
+          if (snapshot.hasError) {
+            return const Center(
+              child: ErrorMessage(),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const TopicsScreen();
+          }
+
+          return const LoginScreen();
+        });
   }
 }
